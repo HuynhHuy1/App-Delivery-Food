f0632f5214c035a4befb89533d30c8a9771c3e46
HuyIT <huyhn2.21it@vku.udn.vn>
2023-04-24 15:30:36 +0700

    commit project

---
+++ app/src/main/java/com/example/fooddeliveryapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fooddeliveryapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fooddeliveryapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
